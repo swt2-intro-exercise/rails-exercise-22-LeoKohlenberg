a6586f18622a3b17622cdda5ba89065a80c93c24
Leo Kohlenberg <leo.kohlenberg@student.hpi.de>
2022-10-31 16:24:07 +0100

    Failing test for #4

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -11,4 +11,13 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should save the author after submitting the form" do
+    visit new_author_path
+    fill_in 'author[first_name]', with: 'Alan'
+    fill_in 'author[last_name]', with: 'Turing'
+    fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+    expect { Author.find_by(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing') }.not_to raise_error
+  end
 end
