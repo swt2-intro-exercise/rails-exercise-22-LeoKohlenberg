3ac5feb119f3441c033419aa5a229839fb1eb054
Leo Kohlenberg <leo.kohlenberg@student.hpi.de>
2022-11-01 17:17:25 +0100

    Failing test for #7

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Author, type: :model do
     expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
     expect(author.name).to eq('Alan Turing')
   end
+
+  it "should not validate without last name" do
+    author = Author.new(first_name: 'Alan', last_name: '', homepage: 'http://wikipedia.org/Alan_Turing')
+    expect(author).not_to be_valid
+  end
 end
